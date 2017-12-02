fc437696ed56795c250f53783d7cf2f333382854
Compilenix <Compilenix@compilenix.org>
2017-12-02 18:46:47 +0100

    echo server addresses on server start to console

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 require("use-strict");
+const os = require("os");
 const port = 9991;
 new (require("./Server.js"))(port).start();
+console.log(`Server address to dump data into: http://${os.hostname}:${port}\nServer adress to receive dumped data via websockets: http://${os.hostname}:${port + 1}`);
